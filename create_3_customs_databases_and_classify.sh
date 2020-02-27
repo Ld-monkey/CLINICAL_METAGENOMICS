3a70e18fb66f0afa812ac7277415220be08adb3b
lfourteau91@gmail.com <lfourteau91@gmail.com>
2020-02-27 15:56:50 +0100

    decoupage des scripts

--- create_3_customs_databases_and_classify.sh
+++ create_3_customs_databases_and_classify.sh
@@ -3,8 +3,8 @@
 #$ -S /bin/bash
 #$ -N build_fda_database
 #$ -cwd
-#$ -o out2_clean.out
-#$ -e err2_clean.err
+#$ -o out_3_fastq.out
+#$ -e err_3_fastq.err
 #$ -q short.q
 #$ -l h_rt=48:00:00
 #$ -pe thread 30
@@ -34,27 +34,41 @@ PATH=$PATH:/data2/apps/blastplus/2.2.31/bin
 #bash classify_set_sequences.sh /data1/scratch/masalm/Valid_Mg_Groute/190710-Nextseq-bact/FASTQ database_clean 30 output_reads_clean_FDA
 
 # Build FDA database + RefSeqHuman + Virus
-echo "Build FDA database + RefSeqHuman + Virus"
-bash FDA_RefSeq_Human_Viral.sh -ref FDA_ARGOS_all_ncbi_genomes-2020-02-04 -database database_fda_refseq_human_viral -threads 30
+#echo "Build FDA database + RefSeqHuman + Virus"
+#bash FDA_RefSeq_Human_Viral.sh -ref ALL_RAW_FILES_GENOMES_FDA_ARGOS-2020-02-04 -database database_fda_refseq_human_viral -threads 30
 
 # Classify set of sequences (reads).
-echo "Classify set of sequences for FDA database + RefSeqHuman + Virus"
-bash classify_set_sequences.sh /data1/scratch/masalm/Valid_Mg_Groute/190710-Nextseq-bact/FASTQ database_fda_refseq_human_viral 30 output_reads_clean_FDA_refseq_human_viral
+#echo "Classify set of sequences for FDA database + RefSeqHuman + Virus"
+#bash classify_set_sequences.sh /data1/scratch/masalm/Valid_Mg_Groute/190710-Nextseq-bact/FASTQ database_fda_refseq_human_viral 30 output_reads_clean_FDA_refseq_human_viral
+
 
 # Preprocess on reads.
-bash lauchPreprocess.sh {folder_reads_preprocess}
+#bash lauchPreprocess.sh {folder_reads_preprocess}
+
+# Change fq extention to fastq.
+fq_extention=$(ls /data1/scratch/masalm/LUDOVIC/METAGENOMICS/PAIRED_SAMPLES_ADN/*.fq 2> /dev/null | wc -l)
+if [ "$fq_extention" != 0 ]
+then
+    echo "Change .fq extention to .fastq"
+    for file in *.fq; do
+        mv "$file" "$(basename "$file" ).fastq"
+    done
+else
+    echo "All files are already in fastq format."
+fi
+
 
 # Classify RefSeq set of sequences (reads).
 echo "Classify set of sequences for RefSeq"
-bash classify_set_sequences.sh /path/prepocess/fasta /data2/fdp/kraken 30 output_prepocess_reads_RefSeq
+bash classify_set_sequences.sh /data1/scratch/masalm/LUDOVIC/METAGENOMICS/PAIRED_SAMPLES_ADN /data2/fdb/kraken 30 output_prepocess_reads_RefSeq
 
 # Classify set of sequences (reads) of prepocess FDA dust
 echo "Classify set of sequences for FDA Dust"
-bash classify_set_sequences.sh /path/prepocess/fasta database_clean 30 output_prepocess_reads_clean_FDA
+bash classify_set_sequences.sh /data1/scratch/masalm/LUDOVIC/METAGENOMICS/PAIRED_SAMPLES_ADN DB_FDA_ARGOS_NO_LOW_COMPLEXITY 30 output_prepocess_reads_clean_FDA
 
 #  Classify set of sequences (reads) on preprocess on FDA database + RefSeqHuman + Virus
 echo "Classify set of sequences for preprocess FDA database + RefSeqHuman + Virus"
-bash classify_set_sequences.sh /path/prepocess/fasta database_fda_refseq_human_viral 30 output_preprocess_reads_clean_FDA_refseq_human_viral
+bash classify_set_sequences.sh /data1/scratch/masalm/LUDOVIC/METAGENOMICS/PAIRED_SAMPLES_ADN database_fda_refseq_human_viral 30 output_preprocess_reads_clean_FDA_refseq_human_viral
 
 # Purge module
 module unload kraken
