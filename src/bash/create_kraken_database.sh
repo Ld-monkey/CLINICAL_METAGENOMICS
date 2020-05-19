4d1bafbddd9f5d1fc363e7373aab6d3557544d84
Coco <coco@Basic.ace>
2020-05-19 18:28:15 +0200

    create mycocosm db

--- src/bash/create_kraken_database.sh
+++ src/bash/create_kraken_database.sh
@@ -7,8 +7,99 @@
 # taxo.k2d: Contains taxonomy information used to build the database.
 
 # We don't want to create standard database because we create a custom database.
-# e.g create_kraken_database.sh -ref ../../data/FDA_ARGOS \
+# e.g bash create_kraken_database.sh \
+#    -ref ../../data/FDA_ARGOS \
 #    -database /output_FDA_ARGOS -thread 1
+# Official documentation : https://ccb.jhu.edu/software/kraken2/index.shtml?t=manual
+
+# Function to check the correct -type_db parameter.
+function check_type_database {
+    if [[ $TYPE_DATABASE = "archaea" ]] \
+           ||  [[ $TYPE_DATABASE = "bacteria" ]] \
+           ||  [[ $TYPE_DATABASE = "plasmid" ]] \
+           ||  [[ $TYPE_DATABASE = "viral" ]]  \
+           ||  [[ $TYPE_DATABASE = "human" ]] \
+           ||  [[ $TYPE_DATABASE = "fungi" ]] \
+           ||  [[ $TYPE_DATABASE = "plant" ]] \
+           ||  [[ $TYPE_DATABASE = "protozoa" ]] \
+           ||  [[ $TYPE_DATABASE = "nr" ]] \
+           ||  [[ $TYPE_DATABASE = "nt" ]] \
+           ||  [[ $TYPE_DATABASE = "env_nr" ]] \
+           ||  [[ $TYPE_DATABASE = "env_nt" ]] \
+           ||  [[ $TYPE_DATABASE = "UniVec" ]] \
+           ||  [[ $TYPE_DATABASE = "UniVec_Core" ]]
+    then
+        echo "From https://ccb.jhu.edu/software/kraken2/index.shtml?t=manual#custom-databases"
+        echo "Correct parameter -type_db $TYPE_DATABASE"
+        case $TYPE_DATABASE in
+
+            archaea)
+                echo "*   archaea: RefSeq complete archaeal genomes/proteins"
+                ;;
+            bacteria)
+                echo "*   bacteria: RefSeq complete bacterial genomes/proteins"
+                ;;
+            plasmid)
+                echo "*   plasmid: RefSeq plasmid nucleotide/protein sequences"
+                ;;
+            viral)
+                echo "*   viral: RefSeq complete viral genomes/proteins"
+                ;;
+            human)
+                echo "*   human: GRCh38 human genome/proteins"
+                ;;
+            fungi)
+                echo "*   fungi: RefSeq complete fungal genomes/proteins"
+                ;;
+            plant)
+                echo "*   plant: RefSeq complete plant genomes/proteins"
+                ;;
+            protozoa)
+                echo "*   protozoa: RefSeq complete protozoan genomes/proteins"
+                ;;
+            nr)
+                echo "*   nr: NCBI non-redundant protein database"
+                ;;
+            nt)
+                echo "*   nt: NCBI non-redundant nucleotide database"
+                ;;
+            env_nr)
+                echo "*   env_nr: NCBI non-redundant protein database with sequences from large environmental sequencing projects"
+                ;;
+            env_nt)
+                echo "*   env_nt: NCBI non-redundant nucleotide database with sequences from large environmental sequencing projects"
+                ;;
+            UniVec)
+                echo "*   UniVec: NCBI-supplied database of vector, adapter, linker, and primer sequences that may be contaminating sequencing projects and/or assemblies"
+                ;;
+            UniVec_Core)
+                echo "*   UniVec_Core: A subset of UniVec chosen to minimize false positive hits to the vector database"
+                ;;
+        esac
+    else
+        echo "Take care about official documentation in https://ccb.jhu.edu/software/kraken2/index.shtml?t=manual#custom-databases"
+        echo "-type_db parameter doesn't correspond to following list :"
+        echo -e "
+           *   archaea: RefSeq complete archaeal genomes/proteins
+           *   bacteria: RefSeq complete bacterial genomes/proteins
+           *   plasmid: RefSeq plasmid nucleotide/protein sequences
+           *   viral: RefSeq complete viral genomes/proteins
+           *   human: GRCh38 human genome/proteins
+           *   fungi: RefSeq complete fungal genomes/proteins
+           *   plant: RefSeq complete plant genomes/proteins
+           *   protozoa: RefSeq complete protozoan genomes/proteins
+           *   nr: NCBI non-redundant protein database
+           *   nt: NCBI non-redundant nucleotide database
+           *   env_nr: NCBI non-redundant protein database with sequences from large environmental sequencing projects
+           *   env_nt: NCBI non-redundant nucleotide database with sequences from large environmental sequencing projects
+           *   UniVec: NCBI-supplied database of vector, adapter, linker, and primer sequences that may be contaminating sequencing projects and/or assemblies
+           *   UniVec_Core: A subset of UniVec chosen to minimize false positive hits to the vector database
+            "
+        echo "Error in -type parameter"
+
+        exit 1
+    fi    
+}
 
 PROGRAM=create_kraken_database.sh
 VERSION=1.0
@@ -21,14 +112,15 @@ __DESCRIPTION__
 OPTIONS=$(cat << __OPTIONS__
 
 ## OPTIONS ##
-    -ref      (Input) folder path of other sequences file fna                                              *FILE: sequences.fna
-    -database (Input) folder path to create or view the database                                           *DIR: database
-    -threads  (Input) the number of threads to build the datab ase faster                                   *INT: 6
+    -path_seq  (Input) Folder path of other sequences in fna or fasta files                                 *DIR: sequences
+    -path_db   (Input) Folder path to create or view the database                                           *DIR: database
+    -type_db   (Input) Which reference librairie for db (choices: viral, fungi, bacteria) see offical doc   *STR: fungi
+    -threads   (Input) The number of threads to build the datab ase faster                                  *INT: 6
 __OPTIONS__
        )
 
 # default options:
-threads=1f
+threads=1
 
 USAGE ()
 {
@@ -56,16 +148,21 @@ BAD_OPTION ()
 while [ -n "$1" ]; do
     case $1 in
         -h)                    USAGE      ; exit 0 ;;
-        -ref)          PATH_SEQUENCES=$2    ; shift 2; continue ;;
-  	    -database)             DBNAME=$2    ; shift 2; continue ;;
-    	  -threads)             threads=$2    ; shift 2; continue ;;
+        -path_seq)             PATH_SEQUENCES=$2    ; shift 2; continue ;;
+  	    -path_db)              DBNAME=$2            ; shift 2; continue ;;
+        -type_db)              TYPE_DATABASE=$2     ; shift 2; continue ;;
+    	  -threads)              threads=$2           ; shift 2; continue ;;
         *)       BAD_OPTION $1;;
     esac
 done
 
-echo " ---- Kraken 2 ---- "
+echo " ---- Create Kraken 2 Database ---- "
 echo $PATH_SEQUENCES
 echo $DBNAME
+
+# Check the correct parameter for -type_db
+check_type_database
+
 echo $threads
 
 # Check if the folder for sequences exists.
@@ -82,7 +179,7 @@ if [ -d $DBNAME ]
 then
     echo "$DBNAME folder already exits."
 else
-    mkdir database
+    mkdir $DBNAME
     echo "Create folder database "
 fi
 
@@ -91,25 +188,10 @@ unzip_files=$(ls $PATH_SEQUENCES/*.gz 2> /dev/null | wc -l)
 if [ "$unzip_files" != "0" ]
 then
     echo "Unzip all files"
-    gunzip $PATH_SEQUENCES/*.fna.gz
+    gunzip --verbose $PATH_SEQUENCES/*.gz
     echo "$PATH_SEQUENCES Unzip done !"
 else
     echo "$PATH_SEQUENCES Files are already decompressed"
-fi
-
-# Check if the taxonomy files from database are already decompressed.
-taxonomy_unzip=$(ls $DBNAME/taxonomy/*.gz 2> /dev/null | wc -l)
-if [ "$taxonomy_unzip" != "0" ]
-then
-    echo "Unzip all files"
-    gunzip $DBNAME/taxonomy/*.gz
-    echo "$DBNAME Unzip done !"
-    tar zcvf $DBNAME/taxonomy/*.tar
-    rm $DBNAME/taxonomy/*.tar
-    echo "$DBNAME tar unziped"
-    echo "files tar removed"
-else
-    echo "$DBNAME Files are already decompressed"
 fi
 
 # 1) To build a custom database we need
@@ -128,11 +210,26 @@ else
     echo "NCBI taxonomy is already exists."
 fi
 
+# Check if the taxonomy files from database are already decompressed.
+taxonomy_unzip=$(ls $DBNAME/taxonomy/*.gz 2> /dev/null | wc -l)
+if [ "$taxonomy_unzip" != "0" ]
+then
+    echo "Unzip all files"
+    gunzip $DBNAME/taxonomy/*.gz
+    echo "$DBNAME Unzip done !"
+    tar zcvf $DBNAME/taxonomy/*.tar
+    rm $DBNAME/taxonomy/*.tar
+    echo "$DBNAME tar unziped"
+    echo "files tar removed"
+else
+    echo "$DBNAME Files are already decompressed"
+fi
+
 # 2) We can add other sequences in the database from fasta files
 # maybe all genome in database.
 
-# viral: RefSeq complete viral genomes/proteins
-kraken2-build --download-library viral --db $DBNAME
+# Download the kraken 2 taxonomy library.
+kraken2-build --download-library $TYPE_DATABASE --db $DBNAME
 
 # Before adding the sequences to the library, check if the database is not already created hash.k2d + opts.k2d + taxo.k2d .
 if [ -f $DBNAME/hash.k2d ] && [ -f $DBNAME/opts.k2d ] && [ -f $DBNAME/taxo.k2d ]
@@ -141,13 +238,32 @@ then
     echo "All jobs are done in this session !"
 else
     echo "Let's create database"
-    # Third argument to define the path to all sequences from other database
-    PATH_OTHER_SEQUENCES=$3
-    echo "Adding reference to Kraken 2 library"
-    for fasta_file in $PATH_SEQUENCES/*.fna
-    do
-        kraken2-build --add-to-library $fasta_file --db $DBNAME
-    done
+
+    # Check if format is .fna or .fasta .
+    is_fna_format=$(ls $PATH_SEQUENCES/*.fna 2> /dev/null | wc -l)
+    is_fasta_format=$(ls $PATH_SEQUENCES/*.fasta 2> /dev/null | wc -l)
+
+    if [ "$is_fna_format" != "0" ]
+    then
+        echo "Adding reference to Kraken 2 library"
+        for fna_file in $PATH_SEQUENCES/*.fna
+        do
+            kraken2-build --add-to-library $fna_file --db $DBNAME
+        done
+    else
+        echo "No *.fna format"
+    fi
+
+    if [ "$is_fasta_format" != "0" ]
+    then
+        echo "Adding reference to Kraken 2 library"
+        for fasta_file in $PATH_SEQUENCES/*.fasta
+        do
+            kraken2-build --add-to-library $fasta_file --db $DBNAME
+        done
+    else
+        echo "No *.fasta format"
+    fi
 
     # 3) Once library is finalized we need to build the database.
     # parameters --threads to reduce build time.
--- src/cluster/cluster_create_kraken_databases.sh
+++ src/cluster/cluster_create_kraken_databases.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -N build_fda_database
+#$ -N build_kraken_2_db
 #$ -cwd
-#$ -o out_3_fastq.out
-#$ -e err_3_fastq.err
+#$ -o out_build_kraken_db.out
+#$ -e err_build_kraken_db.err
 #$ -q short.q
 #$ -l h_rt=48:00:00
 #$ -pe thread 30
@@ -18,10 +18,13 @@ echo "SGE O WORKDIR: $SGE_O_WORKDIR"
 echo "SGE TASK ID: $SGE_TASK_ID"
 echo "NSLOTS: $NSLOTS"
 
-# A shell script to create 2 databases :
+# A shell script to create kraken 2 databases :
 # * FDA-ARGOS database
 # * FDA-ARGOS + RefSeqHuman + Virus database
 # * RefSeq already exists.
+# * Mycocosm fungi CDS
+
+# It's not necessary to re-download the kraken taxonomy for each database.
 
 # Enable conda environment
 conda active metagenomic_env
@@ -29,22 +32,30 @@ conda active metagenomic_env
 # Thread variable.
 THREAD=7
 
-# Variable of path of sample paired reads
-SAMPLE_READS=../../data/reads/PAIRED_SAMPLES_ADN
+# # Build FDA-ARGOS database with low complexity sequences.
+# echo "Build FDA-ARGOS kraken database"
+# bash ../bash/create_kraken_database.sh \
+#      -path_seq ../../data/raw_sequences/ALL_RAW_FILES_GENOMES_FDA_ARGOS-2020-02-04 \
+#      -path_db ../../data/databases/fda_argos_kraken_db_with_low_complexity \
+#      -type_db viral \
+#      -threads $THREAD
 
-# Build FDA-ARGOS database with low complexity sequences.
-echo "Build FDA-ARGOS database"
-bash ../bash/create_kraken_database.sh \
-     -ref ../../data/raw_sequences/ALL_RAW_FILES_GENOMES_FDA_ARGOS-2020-02-04 \
-     -database ../../data/databases/fda_argos_kraken_db_with_low_complexity \
-     -threads $THREAD
+# # Build FDA-ARGOS + RefSeqHuman + Virus in one database.
+# echo "Build FDA database + RefSeqHuman + Virus kraken database"
+# bash  ../bash/create_kraken_database.sh \
+#       -path_seq \
+#       -path_db ../../data/databases/database_fda_refseq_human_viral \
+#       -type_db viral \
+#       -threads $THREAD
 
-# Build FDA-ARGOS + RefSeqHuman + Virus in one database.
-echo "Build FDA database + RefSeqHuman + Virus database"
+# Build kraken database with mycocosm (fungi) coding sequences (CDS)
+echo "Build Mycocosm (fungi) kraken database."
 bash  ../bash/create_kraken_database.sh \
-      -ref  \
-      -database database_fda_refseq_human_viral \
+      -path_seq ../../data/raw_sequences/mycocosm_fungi_CDS_19_05_2020 \
+      -path_db ../../data/databases/mycocosm_fungi_CDS_kraken_database_19_05_2020 \
+      -type_db fungi \
       -threads $THREAD
+echo "Kraken 2 - Mycocosm (fungi) database done !"
 
 # Disable conda environment
 conda deactivate
