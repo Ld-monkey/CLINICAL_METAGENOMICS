d3a68da865982eec7a55771068393333191faa90
Zygnematophyce <lfourteau91@gmail.com>
2020-07-20 22:23:28 +0200

    run blastn first time

--- src/bash/classify_set_reads_blast.sh
+++ src/bash/classify_set_reads_blast.sh
@@ -17,8 +17,7 @@ function check_sequence_folder {
     else
         if [ -d ${PATH_SEQUENCES} ]
         then
-            echo $PATH_SEQUENCES
-            echo "$PATH_SEQUENCES folder of sequence exist."
+            echo "`basename $PATH_SEQUENCES` folder of sequence exist."
         else
             echo "Error $PATH_SEQUENCES doesn't exist."
             echo "No sequences for the blast algorithm."
@@ -39,8 +38,7 @@ function check_blast_database_folder {
     else
         if [ -d ${BLAST_DATABASE} ]
         then
-            echo $BLAST_DATABASE
-            echo "$BLAST_DATABASE blast database folder exist."
+            echo "`basename $BLAST_DATABASE` blast database folder exist."
         else
             echo "Error $BLAST_DATABASE doesn't exist."
             echo "We cannot do a blast analysis without a suitable database (see also create_blast_database.sh)."
@@ -57,33 +55,44 @@ function create_output_folder {
 
 
 function blast_all_sequences {
-    for FASTA in PATH_SEQUENCES; do
 
-        # For blast + (http://nebc.nerc.ac.uk/bioinformatics/documentation/blast+/user_manual.pdf)
-        # blastn -task megablast : used to find very similar sequences.
-        # -evalue : Expectation value threshold for saving hits.
-        # -db : File name of BLAST database.
-        # -outfmt : Allows for the specification of the search application’s output format.
-        # -max_target_seqs : Maximum number of aligned sequences to keep from the blast database.
-        # > output 
-        blastn -task megablast \
-               -evalue 10e-10 \
-               -db $BLAST_DATABASE \
-               -num_threads 1 \
-               -outfmt \"7 qseqid sseqid sstart send evalue bitscore slen staxids\" \
-               -max_target_seqs 1 \
-               -max_hsps 1 \
-               > $OUTPUT_BLAST${FASTA%%.*}_blast_temp.txt
+    COMPLETE_OUTPUT=$OUTPUT_BLAST${BASENAME_WITHOUT_EXTENSION}_blast_temp.out
+    # For blast + (http://nebc.nerc.ac.uk/bioinformatics/documentation/blast+/user_manual.pdf)
+    # blastn -task megablast : used to find very similar sequences.
+    # -evalue : Expectation value threshold for saving hits.
+    # -db : File name of BLAST database.
+    # -outfmt : Allows for the specification of the search application’s output format.
+    # -max_target_seqs : Maximum number of aligned sequences to keep from the blast database.
+    # > output
 
-        # Replace all "processed" to d.
-        sed "/\processed\b/d" $OUTPUT_BLAST${%%.*}_blast_temp.txt \
-            > $OUTPUT_BLAST${FASTA%%.*}_blast_temp2.txt
+    # cat $FASTA | parallel --block 1M --recstart '>' --pipe blastn -task megablast -evalue 10e-10 -db data/databases/blast/fda_argos_assembly_blast_db_17_07_2020/makeblastdb -num_threads 1 -outfmt \"7 qseqid sseqid sstart send evalue bitscore slen staxids\" -max_target_seqs 1 -max_hsps 1 > $COMPLETE_OUTPUT
 
-        # tac : concatenate and write files in reverse ?
-        tac $OUTPUT_BLAST${FASTA%%.*}_blast_temp2.txt \
-            | sed '/0 hits/I,+3 d' \
-            | tac > $OUTPUT_BLAST${FASTA%%.*}_blast.txt
-    done
+    # blastn -task megablast \
+    #        -query $FASTA \
+    #        -evalue 10e-10 \
+    #        -db data/databases/blast/fda_argos_assembly_blast_db_17_07_2020/makeblastdb \
+    #        -outfmt \"7 qseqid sseqid sstart send evalue bitscore slen staxids\" \
+    #        -out $COMPLETE_OUTPUT
+
+    blastn -db data/databases/blast/fda_argos_assembly_blast_db_17_07_2020/makeblastdb \
+           -query $FASTA \
+           -task "megablast" \
+           -out $COMPLETE_OUTPUT \
+           -evalue 10e-10 \
+           -outfmt "7 qseqid sseqid sstart send evalue bitscore slen staxids" \
+           -max_hsps 1 \
+           -max_target_seqs 5 \
+
+           -num_threads 7
+
+    # Replace all "processed" to d.
+    # sed "/\processed\b/d" $OUTPUT_BLAST${%%.*}_blast_temp.txt \
+    #     > $OUTPUT_BLAST${FASTA%%.*}_blast_temp2.txt
+
+    # # tac : concatenate and write files in reverse ?
+    # tac $OUTPUT_BLAST${FASTA%%.*}_blast_temp2.txt \
+    #     | sed '/0 hits/I,+3 d' \
+    #     | tac > $OUTPUT_BLAST${FASTA%%.*}_blast.txt
 }
 
 
@@ -105,7 +114,7 @@ __DESCRIPTION__
 OPTIONS=$(cat << __OPTIONS__
 
 ## OPTIONS ##
-    -path_seq    (Input)  Path to the folder that contains the sequences to be aligned.                 *DIR: results/classify_reads/trimmed_classify_fda_argos_with_none_library_02_07_2020/1-MAR-LBA-ADN_S1/convert_fastq_to_fasta/1-MAR-LBA-ADN.fasta
+    -path_seq    (Input)  Path to the folder that contains the sequences to be aligned.                 *DIR: results/classify_reads/trimmed_classify_fda_argos_with_none_library_02_07_2020/1-MAR-LBA-ADN_S1/convert_fastq_to_fasta/
     -path_db     (Input)  Path to local blast database folder. (see create_blast_database.sh)           *DIR: data/refseq_genomics_virus_blast_db_17_07_2020/
     -path_output (Output) The folder of output blast classification.                                    *DIR: results/blast/refseq_result_blast_17_07_2020/
 __OPTIONS__
@@ -157,7 +166,11 @@ check_blast_database_folder
 create_output_folder
 
 # Blast the sequences.
-blast_all_sequences
-
-# Remove intermediate files.
-remove_intermediate_file
+for FASTA in ${PATH_SEQUENCES}*.fasta; do
+    echo "fasta = $FASTA"
+    BASENAME_WITHOUT_EXTENSION=$(basename ${FASTA%%.*} )
+    echo "basename = $BASENAME_WITHOUT_EXTENSION"
+    blast_all_sequences
+done
+# # Remove intermediate files.
+# remove_intermediate_file
