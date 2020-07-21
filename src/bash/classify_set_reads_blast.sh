6d24db3a2e2e56ca8791116baec4f49f4e1941fe
Zygnematophyce <lfourteau91@gmail.com>
2020-07-21 16:31:13 +0200

    classify reads blast with all commands

--- src/bash/classify_set_reads_blast.sh
+++ src/bash/classify_set_reads_blast.sh
@@ -58,15 +58,19 @@ function blast_all_sequences {
 
     COMPLETE_OUTPUT=$OUTPUT_BLAST${BASENAME_WITHOUT_EXTENSION}_blast_temp.out
 
-    # For blast + (http://nebc.nerc.ac.uk/bioinformatics/documentation/blast+/user_manual.pdf)
-    # blastn -task megablast : used to find very similar sequences.
-    # -evalue : Expectation value threshold for saving hits.
+    # For blastn documentation see also :
+    # http://nebc.nerc.ac.uk/bioinformatics/documentation/blast+/user_manual.pdf
     # -db : File name of BLAST database.
+    # -query : the sequence to compare.
+    # -task : megablast used to find very similar sequences.
+    # -out : output file.
+    # -evalue : Expectation value threshold for saving hits.
     # -outfmt : Allows for the specification of the search application’s output format.
+    # -max_hsps : Maximum number of HSPs (alignments) to keep for any single query-subject pair.
     # -max_target_seqs : Maximum number of aligned sequences to keep from the blast database.
-    # -out : output
-
-    blastn -db data/databases/blast/fda_argos_assembly_blast_db_17_07_2020/makeblastdb \
+    # -num_threads : Number of threads.
+    
+    blastn -db $BLAST_DATABASE$NAME_DATABASE \
            -query $FASTA \
            -task "megablast" \
            -out $COMPLETE_OUTPUT \
@@ -74,27 +78,33 @@ function blast_all_sequences {
            -outfmt "7 qseqid sseqid sstart send evalue bitscore slen staxids" \
            -max_hsps 1 \
            -max_target_seqs 5 \
-           -num_threads 7
+           -num_threads $THREADS
 
     echo "Blast done !"
 
-    # Replace all "processed" to d.
-    # sed "/\processed\b/d" $OUTPUT_BLAST${%%.*}_blast_temp.txt \
-    #     > $OUTPUT_BLAST${FASTA%%.*}_blast_temp2.txt
+    echo "Delete last line of file."
+    # Delete last line of file.
+    sed "/\processed\b/d" $OUTPUT_BLAST${BASENAME_WITHOUT_EXTENSION}_blast_temp.out \
+      > $OUTPUT_BLAST${BASENAME_WITHOUT_EXTENSION}_blast_temp_2.out
+    echo "Delete done !"
 
-    # # tac : concatenate and write files in reverse ?
-    # tac $OUTPUT_BLAST${FASTA%%.*}_blast_temp2.txt \
-    #     | sed '/0 hits/I,+3 d' \
-    #     | tac > $OUTPUT_BLAST${FASTA%%.*}_blast.txt
+    # The goal is to remove 0 hits from the blast output. As the 0 hits are usually
+    # towards the end of the file we read the file upside down with 'tac' command
+    # then we put it right side up on other output.
+    echo "Remove 0 hits from blast output."
+    tac $OUTPUT_BLAST${BASENAME_WITHOUT_EXTENSION}_blast_temp_2.out \
+        | sed '/0 hits/I,+3 d' \
+        | tac > $OUTPUT_BLAST${BASENAME_WITHOUT_EXTENSION}_blast.txt
+    echo "Remove 0 hits done !"
 }
 
 
 function remove_intermediate_file {
     # Remove tempory files.
-    rm -rf ${OUTPUT_BLAST}*_blast_temp.txt
-    rm -rf ${OUTPUT_BLAST}*_blast_temp2.txt
+    rm -rf $OUTPUT_BLAST${BASENAME_WITHOUT_EXTENSION}_blast_temp.out
+    rm -rf $OUTPUT_BLAST${BASENAME_WITHOUT_EXTENSION}_blast_temp_2.out
+    echo "Remove done !"
 }
-
 
 PROGRAM=classify_set_reads_blast.sh
 VERSION=1.0
@@ -107,13 +117,15 @@ __DESCRIPTION__
 OPTIONS=$(cat << __OPTIONS__
 
 ## OPTIONS ##
-    -path_seq    (Input)  Path to the folder that contains the sequences to be aligned.                 *DIR: results/classify_reads/trimmed_classify_fda_argos_with_none_library_02_07_2020/1-MAR-LBA-ADN_S1/convert_fastq_to_fasta/
-    -path_db     (Input)  Path to local blast database folder. (see create_blast_database.sh)           *DIR: data/refseq_genomics_virus_blast_db_17_07_2020/
-    -path_output (Output) The folder of output blast classification.                                    *DIR: results/blast/refseq_result_blast_17_07_2020/
+    -path_seq    (Input)    Path to the folder that contains the sequences to be aligned.                 *DIR: results/classify_reads/trimmed_classify_fda_argos_with_none_library_02_07_2020/1-MAR-LBA-ADN_S1/convert_fastq_to_fasta/
+    -path_db     (Input)    Path to local blast database folder. (see create_blast_database.sh)           *DIR: data/refseq_genomics_virus_blast_db_17_07_2020/
+    -path_output (Output)   The folder of output blast classification.                                    *DIR: results/blast/refseq_result_blast_17_07_2020/
+    -threads     (Optional) The number of threads to classify faster.                                     *INT: 10
 __OPTIONS__
        )
 
 # default options:
+THREADS=10
 
 USAGE ()
 {
@@ -145,9 +157,12 @@ while [ -n "$1" ]; do
         -path_seq)          PATH_SEQUENCES=$2 ; shift 2; continue ;;
         -path_db)           BLAST_DATABASE=$2 ; shift 2; continue ;;
         -path_output)       OUTPUT_BLAST=$2   ; shift 2; continue ;;
+        -threads)           THREADS=$2        ; shift 2; continue ;;
         *)       BAD_OPTION $1;;
     esac
 done
+
+echo "Number of threads : $THREADS"
 
 # Check folder with sequences.
 check_sequence_folder
@@ -158,6 +173,10 @@ check_blast_database_folder
 # Create output folder.
 create_output_folder
 
+# Get the name of blast database.
+NAME_DATABASE=$(ls ${BLAST_DATABASE}*.n* | head -n 1)
+NAME_DATABASE=$(basename ${NAME_DATABASE%%.*})
+
 # Blast the sequences.
 for FASTA in ${PATH_SEQUENCES}*.fasta; do
     echo "fasta = $FASTA"
@@ -167,4 +186,4 @@ for FASTA in ${PATH_SEQUENCES}*.fasta; do
 done
 
 # Remove intermediate files.
-# remove_intermediate_file
+remove_intermediate_file
